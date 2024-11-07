ac0ecf0ef514a7b0595b5d40b0f7f59f50386ba3
sayomaki <sayomayomaki@gmail.com>
2024-11-08 05:32:19 +0800

    Add some comments for the new utility class

--- src/main/java/seedu/address/commons/util/UrlUtil.java
+++ src/main/java/seedu/address/commons/util/UrlUtil.java
@@ -2,13 +2,27 @@ package seedu.address.commons.util;
 
 import javafx.application.HostServices;
 
+/**
+ * Singleton utility class that contains an instance of HostServices from the JavaFX application.
+ * @author sayomaki
+ */
 public class UrlUtil {
+    /** Instance of host services **/
     private static HostServices hostServices;
 
+    /**
+     * Sets the HostServices instance from the JavaFX application
+     * @param hostServices the HostServices instance of the application
+     */
     public static void setHostServices(HostServices hostServices) {
         UrlUtil.hostServices = hostServices;
     }
 
+    /**
+     * Opens the requested URL with the default browser on the user's computer.
+     * This does nothing if the host services instance is not yet setup.
+     * @param url the url to open
+     */
     public static void openUrl(String url) {
         if (hostServices == null) {
             return;
